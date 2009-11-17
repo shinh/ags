559235d8d85eaf0a596663d4a1200f71d27a0b77
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2009-11-17 14:13:41 +0900

    better remounter

--- be/srv/remount.c
+++ be/srv/remount.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <sys/mount.h>
+#include <unistd.h>
 int main() {
+    sync();
     if (umount("/dev/shm") != 0) {
         perror("umount");
-        return 1;
+        // It's not so serious.
+        //return 1;
     }
     if (mount("devshm", "/dev/shm", "tmpfs", MS_MGC_VAL, NULL) != 0) {
         perror("mount");
