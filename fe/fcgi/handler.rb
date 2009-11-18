0becb584717e19838a8681e792a81d8b304512e4
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2009-11-19 03:46:09 +0900

    move read_multipart into handler.rb

--- fe/fcgi/handler.rb
+++ fe/fcgi/handler.rb
@@ -335,4 +335,107 @@ class Handler
     [ret, lr]
   end
 
+  def read_multipart(boundary, content_length)
+    params = Hash.new([])
+    boundary = "--" + boundary
+    quoted_boundary = Regexp.quote(boundary, "n")
+    buf = ""
+    bufsize = 10 * 1024
+
+    # start multipart/form-data
+    @i.binmode if defined? @i.binmode
+    boundary_size = boundary.size + @@eol.size
+    content_length -= boundary_size
+    status = @i.read(boundary_size)
+    if nil == status
+      raise EOFError, "no content body"
+    elsif boundary + @@eol != status
+      raise EOFError, "bad content body"
+    end
+
+    loop do
+      head = nil
+      if 10240 < content_length
+        require "tempfile"
+        body = Tempfile.new("CGI")
+      else
+        begin
+          require "stringio"
+          body = StringIO.new
+        rescue LoadError
+          require "tempfile"
+          body = Tempfile.new("CGI")
+        end
+      end
+      body.binmode if defined? body.binmode
+
+      until head and /#{quoted_boundary}(?:#{@@eol}|--)/n.match(buf)
+
+          if (not head) and /#{@@eol}#{@@eol}/n.match(buf)
+              buf = buf.sub(/\A((?:.|\n)*?#{@@eol})#{@@eol}/n) do
+            head = $1.dup
+            ""
+          end
+            next
+          end
+
+        if head and ( (@@eol + boundary + @@eol).size < buf.size )
+          body.print buf[0 ... (buf.size - (@@eol + boundary + @@eol).size)]
+          buf[0 ... (buf.size - (@@eol + boundary + @@eol).size)] = ""
+        end
+
+        c = if bufsize < content_length
+              @i.read(bufsize)
+            else
+              @i.read(content_length)
+            end
+        if c.nil? || c.empty?
+          raise EOFError, "bad content body"
+        end
+        buf.concat(c)
+        content_length -= c.size
+      end
+
+      buf = buf.sub(/\A((?:.|\n)*?)(?:[\r\n]{1,2})?#{quoted_boundary}([\r\n]{1,2}|--)/n) do
+        body.print $1
+        if "--" == $2
+          content_length = -1
+        end
+        ""
+      end
+
+      body.rewind
+
+      /Content-Disposition:.* filename="?([^\";]*)"?/ni.match(head)
+      filename = ($1 or "")
+      if /Mac/ni.match(@e['HTTP_USER_AGENT']) and
+          /Mozilla/ni.match(@e['HTTP_USER_AGENT']) and
+          (not /MSIE/ni.match(@e['HTTP_USER_AGENT']))
+        filename = CGI::unescape(filename)
+      end
+
+      /Content-Type: (.*)/ni.match(head)
+      content_type = ($1 or "")
+
+      (class << body; self; end).class_eval do
+        alias local_path path
+        define_method(:original_filename) {filename.dup.taint}
+        define_method(:content_type) {content_type.dup.taint}
+      end
+
+      /Content-Disposition:.* name="?([^\";]*)"?/ni.match(head)
+      name = $1.dup
+
+      if params.has_key?(name)
+        params[name].push(body)
+      else
+        params[name] = [body]
+      end
+      break if buf.size == 0
+      break if content_length === -1
+    end
+
+    params
+  end # read_multipart
+
 end
