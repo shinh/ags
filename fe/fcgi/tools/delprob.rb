b7369e81f049516c3034feb3fbd8875cefc0c368
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2011-02-07 02:19:25 +0900

    warning for wrong problem names

--- fe/fcgi/tools/delprob.rb
+++ fe/fcgi/tools/delprob.rb
@@ -3,8 +3,13 @@ db=PStore.new('db/problem.db')
 db.transaction do
   prob = ARGV[0]
   i = db['root'].index(prob)
+  if !i
+    puts 'not found'
+    #p db['root']
+    exit 1
+  end
   p db['root'][i]
-  p Time.at(db['deadline'][i])
+  p Time.at(db['deadline'][i] || 0)
 
   if ARGV[1] == '-f'
     db['root'].delete_at(i)
