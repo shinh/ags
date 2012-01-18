7abc8e6bc4664f4938af309551741969647cd74f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2012-01-19 00:02:52 +0900

    Notice phpgolf.org

--- fe/fcgi/index.rb
+++ fe/fcgi/index.rb
@@ -115,6 +115,7 @@ Mark Byers kindly prepared a <a href="http://sites.google.com/site/codegolfingti
     puts %Q(
 <h2>News</h2>
 <ul>
+<li>For PHP golfers: visit <a href="http://www.phpgolf.org/">phpGolf.org</a> for more competitive golf courses!
 <li>Now PHP doesn't report E_NOTICE error so you may see less annoying timeouts.
 <li>Downgraded python 2.7 => 2.5.5 as 2.7 is not backward compatible with the previous version (2.5.4).
 <li>The golf server has migrated to a VPS (http://vps.sakura.ad.jp/).
