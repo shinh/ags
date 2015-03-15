905e1e98459755a01053065e2ecb2250f539a959
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2015-03-15 16:58:30 +0900

    Update the version info of rakudo

--- fe/fcgi/index.rb
+++ fe/fcgi/index.rb
@@ -115,6 +115,7 @@ Mark Byers kindly prepared a <a href="http://sites.google.com/site/codegolfingti
     puts %Q(
 <h2>News</h2>
 <ul>
+<li>Updated the interpreter of Perl6 (2015-03-15). Old solutions may stop working but the preious version was way too old.
 <li>Updated the interpreter of Aheui, so you should be able to solve problems with inputs now.
 <li>Added <a href="http://esolangs.org/wiki/Aheui">Aheui</a>.
 <li>Now we pass -s flag to jq. I may remove some entries which don't pass without -s flag anymore.
