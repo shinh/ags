66bd94981436ba1b77fe97bd0830d51600096458
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2026-01-14 10:29:55 +0900

    Announce iogii update

--- fe/fcgi/index.rb
+++ fe/fcgi/index.rb
@@ -116,6 +116,7 @@ Mark Byers kindly prepared a <a href="http://sites.google.com/site/codegolfingti
     puts %Q(
 <h2>News</h2>
 <ul>
+<li>2026-01-14: Updated iogii to 1.1.1.
 <li>2025-07-07: Updated iogii to 1.1.
 <li>2025-04-30: Updated iogii to 1.0.1.
 <li>2025-03-24: Updated iogii to 0.3 (beta).
